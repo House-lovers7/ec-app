7dfcc3c07b14caf04bb23741db6d563eca7d124b
House-lovers7 <taisuke.got@gmail.com>
2022-02-10 11:42:39 +0900

    不要なコードの削除

--- src/reducks/store/initialState.js
+++ src/reducks/store/initialState.js
@@ -10,26 +10,3 @@ const initialState = {
 };
 
 export default initialState
-
-
-
-export const initialState = {
-  loading: {
-      state: false,
-      text: ""
-  },
-  products: {
-      list: []
-  },
-  users: {
-      cart: [],
-      customer_id: "",
-      email: "",
-      isSignedIn: false,
-      orders: [],
-      payment_method_id: "",
-      role: "",
-      uid: "",
-      username: ""
-  }
-};
